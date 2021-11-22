d75115414a55bbe1a25eb2561e8284dc0bd06c1c
pranu2502 <spranavreddy25@gmail.com>
2021-11-23 01:20:29 +0530

    first commit

---
+++ prog.py
@@ -0,0 +1,7 @@
+def add(n1, n2):
+    return (n1 + n2)
+
+def div(n1, n2):
+    if(n2!=0):
+        return (n1 / n2)
+    return "Inf"
---
+++ test.py
@@ -0,0 +1,28 @@
+from prog import add,div
+
+def test():
+  res = add(12,23)
+  expected = 35
+  assert res == expected
+  print("1. add(12,23)")
+  print("pass")
+
+  res = div(12,2)
+  expected = 6
+  assert res == expected
+  print("2. add(12,2)")
+  print("pass")
+
+  res = div(12,0)
+  expected = "Inf"
+  assert res == expected
+  print("3. div(12,0)")
+  print("pass")
+
+
+
+if __name__ == "__main__":
+  try:
+    test()
+  except Exception as e:
+    print("An error ocured: " + e)
